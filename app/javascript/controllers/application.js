b5742e81474609bd9962d0cb4f7182f5cbf23e55
Fernanda Yoshida <yoshida.fernanda@gmail.com>
2025-09-05 19:59:12 -0300

    arrumando bug v5

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,11 +1,5 @@
 import { Application } from "@hotwired/stimulus"
-import PreventMultiSubmitController from "./prevent_multi_submit_controller"
 
-const application = Application.start()
+export const application = Application.start()
 application.debug = false
 window.Stimulus = application
-
-// registre aqui (ou no controllers/index.js, se preferir)
-application.register("prevent-multi-submit", PreventMultiSubmitController)
-
-export { application }
